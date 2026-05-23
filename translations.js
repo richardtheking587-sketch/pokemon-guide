e0de3fc64d69eb20080ea4b2486082c336ea79aa
richardtheking587-sketch <richardtheking587@gmail.com>
2026-05-23 16:35:24 -0300

    Update translations.js

--- translations.js
+++ translations.js
@@ -1,13 +1,37 @@
 const translations = {
+    // Tradução dos Tipos
     types: {
-        normal: 'Normal', fire: 'Fogo', water: 'Água', grass: 'Grama', electric: 'Elétrico',
-        ice: 'Gelo', fighting: 'Lutador', poison: 'Veneno', ground: 'Terra', flying: 'Voador',
-        psychic: 'Psíquico', bug: 'Inseto', rock: 'Pedra', ghost: 'Fantasma', dragon: 'Dragão',
-        dark: 'Sombrio', steel: 'Aço', fairy: 'Fada'
+        normal: 'Normal',
+        fire: 'Fogo',
+        water: 'Água',
+        grass: 'Grama',
+        electric: 'Elétrico',
+        ice: 'Gelo',
+        fighting: 'Lutador',
+        poison: 'Veneno',
+        ground: 'Terra',
+        flying: 'Voador',
+        psychic: 'Psíquico',
+        bug: 'Inseto',
+        rock: 'Pedra',
+        ghost: 'Fantasma',
+        dragon: 'Dragão',
+        dark: 'Sombrio',
+        steel: 'Aço',
+        fairy: 'Fada'
     },
+    // Tradução das Estatísticas
     stats: {
-        hp: 'HP', attack: 'Ataque', defense: 'Defesa',
-        'special-attack': 'Atq. Especial', 'special-defense': 'Def. Especial', speed: 'Velocidade'
+        hp: 'HP',
+        attack: 'Ataque',
+        defense: 'Defesa',
+        'special-attack': 'Atq. Especial',
+        'special-defense': 'Def. Especial',
+        speed: 'Velocidade'
     }
 };
-function translate(cat, key) { return (translations[cat] && translations[cat][key.toLowerCase()]) || key; }
+
+// Função que o script usa para traduzir
+function translate(category, key) {
+    return (translations[category] && translations[category][key.toLowerCase()]) || key;
+}
